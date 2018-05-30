2699346c12e66508c5a8875bdbb619783cdc2b10
atul9911 <atul.agrawal9911@gmail.com>
2018-05-30 23:56:41 +0530

    log directory changed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,8 +15,8 @@ module.exports = {
     },
     "exec_mode": "fork",
     "instances": 1,
-    "error_file": path.join(__dirname, '../../log/', "err.log"),
-    "out_file": path.join(__dirname, '../../log/', "out.log"),
+    "error_file": path.join(__dirname, '../log/', "err.log"),
+    "out_file": path.join(__dirname, '../log/', "out.log"),
     "log_date_format": "YYYY-MM-DD HH:mm Z"
   }]
 
